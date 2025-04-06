d423a4557efa6a0f32a1f42a9c9e1b31d92cc828
MamoruHikari <173257494+MamoruHikari@users.noreply.github.com>
2025-04-06 16:52:36 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import emojipedia from './emojipedia'
+import Card from './Card'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(emoji => (
+          <Card 
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+          />
+        ))}
+      </dl>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+function Card({ emoji, name, meaning }) {
+    return (
+      <div className="term">
+        <dt>
+          <span className="emoji" role="img" aria-label={name}>
+            {emoji}
+          </span>
+          <span>{name}</span>
+        </dt>
+        <dd>
+          {meaning}
+        </dd>
+      </div>
+    )
+}
+  
+  export default Card
+  
